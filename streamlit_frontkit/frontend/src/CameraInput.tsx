78edbfb726fd451480079060cd0d39809f58ccf1
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-09-16 00:21:35 +0900

    Create <CameraInput />

---
+++ streamlit_frontkit/frontend/src/CameraInput.tsx
@@ -0,0 +1,76 @@
+import React, { useCallback, useState, useEffect, useRef } from "react";
+
+declare namespace CameraInput {
+  export interface Props {
+    onFrame: (bitmap: ImageBitmap) => void | Promise<void>;
+  }
+}
+
+const CameraInput: React.VFC<CameraInput.Props> = (props) => {
+  const [playing, setPlaying] = useState(false);
+
+  const onFrame = props.onFrame;
+
+  useEffect(() => {
+    if (!playing) {
+      return;
+    }
+
+    const videoElem = document.createElement("video");
+
+    let lastFrameTime: number | null = null;
+    const onAnimationFrame = async () => {
+      if (!videoElem.paused && videoElem.currentTime !== lastFrameTime) {
+        lastFrameTime = videoElem.currentTime;
+        const bitmap = await createImageBitmap(videoElem);
+        await onFrame(bitmap);
+      }
+
+      waitForNextAnimationFrame();
+    };
+
+    const waitForNextAnimationFrame = () => {
+      window.requestAnimationFrame(function (time) {
+        onAnimationFrame();
+      });
+    };
+
+    let stream: MediaStream | null = null;
+    const constraints: MediaStreamConstraints = { video: true };
+    navigator.mediaDevices.getUserMedia(constraints).then((_stream) => {
+      stream = _stream;
+      videoElem.srcObject = stream;
+      videoElem.onloadedmetadata = function () {
+        videoElem.play();
+        waitForNextAnimationFrame();
+      };
+    });
+
+    return () => {
+      // Clean up function
+      videoElem.pause();
+      videoElem.remove();
+
+      stream?.getTracks().forEach(function (track) {
+        track.stop();
+      });
+
+      setPlaying(false);
+    };
+  }, [playing, onFrame]);
+
+  const requestPlay = useCallback(() => {
+    setPlaying(true);
+  }, []);
+  const requestPause = useCallback(() => {
+    setPlaying(false);
+  }, []);
+
+  return playing ? (
+    <button onClick={requestPause}>Pause</button>
+  ) : (
+    <button onClick={requestPlay}>Play</button>
+  );
+};
+
+export default CameraInput;
